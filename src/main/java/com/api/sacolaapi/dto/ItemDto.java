5d0e85f919ae11b40a682b4ab06de456bcb718d9
andersonhsporto <anderson.higo2@gmail.com>
2022-09-30 02:30:33 -0300

    add: ItemDto

---
+++ src/main/java/com/api/sacolaapi/dto/ItemDto.java
@@ -0,0 +1,20 @@
+package com.api.sacolaapi.dto;
+
+import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable
+@NoArgsConstructor
+public class ItemDto {
+
+  private Long produtoId;
+  private int quantidade;
+  private Long sacolaId;
+
+}
